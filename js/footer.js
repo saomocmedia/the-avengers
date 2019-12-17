eabfda8836f7aa5c5cfde8424bbf2184c1c48f37
Đức Hiệp <Hieppdth1908068@fpt.edu.vn>
2019-12-17 20:11:34 +0700

    duchiepp

--- js/footer.js
+++ js/footer.js
@@ -1,42 +1,45 @@
-
-    var footer = "<footer class=\"section footer\">\n" +
-        "\t\t<footer class=\"container\">\n" +
-        "\t\t\t<div class=\"row\">\n" +
-        "\t\t\t<div class=\"col-md-12\">\n" +
-        "\t\t\t\t<div class=\"col-md-3\">\n" +
-        "\t\t\t\t\t<span class=\"title-footer\"><strong>Cộng đồng</strong></span><br />\n" +
-        "\t\t\t\t\t<span class=\"icon-footer\"><a href=\"#\"><i class=\"fab fa-facebook-square\"></i></a></span>\n" +
-        "\t\t\t\t\t<span class=\"icon-footer\"><a href=\"#\"><i class=\"fab fa-google-plus-square\"></i></a></span>\n" +
-        "\t\t\t\t\t<span class=\"icon-footer\"><a href=\"#\"><i class=\"fab fa-twitter-square\"></i></a></span>\n" +
-        "\t\t\t\t</div>\n" +
-        "\t\t\t\t<div class=\"col-md-2\">\n" +
-        "\t\t\t\t\t<p><span class=\"title-footer\"><strong>Thông tin</strong></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Giới thiệu</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Giao hàng</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Điều khoản</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Bảo mật</a></span></p>\n" +
-        "\t\t\t\t</div>\n" +
-        "\t\t\t\t<div class=\"col-md-3\">\n" +
-        "\t\t\t\t\t<p><span class=\"title-footer\"><strong>Sản phẩm</strong></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Trà sữa kiều mạch</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu kem </a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Chocola </a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Machiato</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Sầu Riêng</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Trà Sữa Machiato</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Trà Dao Olong</a></span></p>\n" +
-        "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Machiato</a></span></p>\n" +
-        "\t\t\t\t</div>\n" +
-        "\t\t\t\t<div class=\"col-md-4\">\n" +
-        "\t\t\t\t\t<p><span class=\"title-footer\"><strong>Địa chỉ</strong></span></p>\n" +
-        "\t\t\t\t\t<p class=\"info-footer\"><strong>Address:</strong> Số nhà 21, ngách 105/42/11, ngõ 105 Xuân La, Q. Bắc Từ Liêm, TP. Hà Nội</p>\n" +
-        "\t\t\t\t\t<p class=\"info-footer\"><strong>Telephone number:</strong> 0914 015 911</p>\n" +
-        "\t\t\t\t\t<p class=\"info-footer\"><strong>Email: </strong>avengers.t1909e@fpt.edu.vn</p>\n" +
-        "\t\t\t\t\t<p class=\"link-footer info-footer\"><strong>Website: </strong><a href=\"https://tea-project.herokuapp.com/\" target=\"_blank\">tea-project.herokuapp.com</a></p>\n" +
-        "\t\t\t\t</div>\n" +
-        "\t\t\t</div>\n" +
-        "\t\t\t</div>\n" +
-        "\t\t</footer>\n" +
-        "\t</footer>"
+ var footer = "<section class=\"section\">\n" +
+     "\t<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.1543605749825!2d105.79944021567923!3d21.06649619186317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab3378343ba5%3A0x6031f536c1646048!2zVGnhu4dtIEPGoW0gVmnhu4d0!5e0!3m2!1sen!2s!4v1576328200913!5m2!1sen!2s\"\n" +
+     "\t\t\twidth=\"100%\" height=\"450\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\"></iframe>\n" +
+     "</section>\n" +
+     "<footer class=\"section footer\">\n" +
+     "\t<footer class=\"container\">\n" +
+     "\t\t<div class=\"row\">\n" +
+     "\t\t\t<div class=\"col-md-12\">\n" +
+     "\t\t\t\t<div class=\"col-md-3\">\n" +
+     "\t\t\t\t\t<span class=\"title-footer\"><strong>Cộng đồng</strong></span><br />\n" +
+     "\t\t\t\t\t<span class=\"icon-footer\"><a href=\"#\"><i class=\"fab fa-facebook-square\"></i></a></span>\n" +
+     "\t\t\t\t\t<span class=\"icon-footer\"><a href=\"#\"><i class=\"fab fa-google-plus-square\"></i></a></span>\n" +
+     "\t\t\t\t\t<span class=\"icon-footer\"><a href=\"#\"><i class=\"fab fa-twitter-square\"></i></a></span>\n" +
+     "\t\t\t\t</div>\n" +
+     "\t\t\t\t<div class=\"col-md-2\">\n" +
+     "\t\t\t\t\t<p><span class=\"title-footer\"><strong>Thông tin</strong></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Giới thiệu</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Giao hàng</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Điều khoản</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Bảo mật</a></span></p>\n" +
+     "\t\t\t\t</div>\n" +
+     "\t\t\t\t<div class=\"col-md-3\">\n" +
+     "\t\t\t\t\t<p><span class=\"title-footer\"><strong>Sản phẩm</strong></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Trà sữa kiều mạch</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu kem </a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Chocola </a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Machiato</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Sầu Riêng</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Trà Sữa Machiato</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Trà Dao Olong</a></span></p>\n" +
+     "\t\t\t\t\t<p><span class=\"link-footer\"><a href=\"#\">Đường nâu Machiato</a></span></p>\n" +
+     "\t\t\t\t</div>\n" +
+     "\t\t\t\t<div class=\"col-md-4\">\n" +
+     "\t\t\t\t\t<p><span class=\"title-footer\"><strong>Địa chỉ</strong></span></p>\n" +
+     "\t\t\t\t\t<p class=\"info-footer\"><strong>Address:</strong> Số nhà 21, ngách 105/42/11, ngõ 105 Xuân La, Q. Bắc Từ Liêm, TP. Hà Nội</p>\n" +
+     "\t\t\t\t\t<p class=\"info-footer\"><strong>Telephone number:</strong> 0914 015 911</p>\n" +
+     "\t\t\t\t\t<p class=\"info-footer\"><strong>Email: </strong>avengers.t1909e@fpt.edu.vn</p>\n" +
+     "\t\t\t\t\t<p class=\"link-footer info-footer\"><strong>Website: </strong><a href=\"https://tea-project.herokuapp.com/\" target=\"_blank\">tea-project.herokuapp.com</a></p>\n" +
+     "\t\t\t\t</div>\n" +
+     "\t\t\t</div>\n" +
+     "\t\t</div>\n" +
+     "\t</footer>\n" +
+     "</footer>"
 
 $("body").append(footer);
